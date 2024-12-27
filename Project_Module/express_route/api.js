f713496a2d11d40b1925a0f9167d5e45605a97fe
Suyog-Dev <31378621+Suyog111@users.noreply.github.com>
2024-12-27 07:23:37 +0000

    add get post

--- Project_Module/express_route/api.js
+++ Project_Module/express_route/api.js
@@ -1,7 +1,88 @@
 const express=require("express");
+const short=require("short-uuid");
 
 const app = express();
-//console.log("hello hi suyuog1");
+
+const fs=require("fs");
+
+const strContent=fs.readFileSync("./dev.json","utf-8");
+const userDataStore=JSON.parse(strContent);
+app.get("/api/user", function (req, res) {
+    let msg = "";
+    try {
+        if (userDataStore.legth === 0) {
+           throw new Error( "no user found");
+        } else {
+            msg = userDataStore;
+        }
+        res.status(200).json({
+            status: "success",
+            message: msg
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: "failure",
+            message: error.msg
+        })
+    }
+})
+
+app.use(express.json());
+
+/**create user***/
+app.post("/api/user",function (req,res) {
+    const id = short.generate();
+    const userDetails = res.body;
+    userDetails.id = id;
+
+
+    userDataStore.push(userDetails);
+    
+    const strUserDS= JSON.stringify(userDataStore);
+    fs.writeFileSync("./dev.json",strUserDS);
+
+    res.use(function cb(req,res) {
+        res.status(200).json({
+            status:"successfull",
+            message:`update the user ${id}`
+        })
+    })
+})
+
+
+
+//it is user defined middlewear
+app.use(function cb(req,res) {
+    console.log("before",res.body);
+    next();
+})
+
+//it is inbuilt middlewear
+app.use(express.json())
+
+app.use(function cb(req,res) {
+    console.log("after",req.body);
+    console.log("Hi i will be called everytime");
+    next();
+})
+
+//when request on api/user execute this
+app.post("",function (res,req) {
+    console.log("i am inside post method");
+    res.status(200).json({
+        status:"success",
+        message:"sending res from post method"
+    })
+})
+
+//when request on api/user execute this
+app.get("/api/user",function (req,res) {
+    console.log("i am inside get method");
+    res.status(200).json({
+        status:"success",
+        message:"sending res from get method"
+    })
+})
 
 /**
  * app.use is mtheod given by exress
@@ -9,15 +90,6 @@ const app = express();
  * 1. req: object reqpresenting actual req
  * 2. res: object representing res
  */
-app.use(function cb(req,res) {
-    console.log("inside use function");
-    
-    res.status(200).json({
-        status:"success",
-        message:"got the request"
-
-    })
-})
 
 
 const port = process.env.PORT || 3000;
